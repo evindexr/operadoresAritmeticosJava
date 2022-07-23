40b18cb2b2ac7d368d5a698022db43e5ef6babe4
emilson.rivera <emilson.caballero@gmail.com>
2022-07-22 20:43:21 -0600

    actualizacion operaciones

--- src/repaso/Repaso.java
+++ src/repaso/Repaso.java
@@ -5,6 +5,8 @@
  */
 package repaso;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -16,8 +18,50 @@ public class Repaso {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Primera linea progra2");
-        System.out.println("prueba de update");
+        
+        
+        
+        //Cree una aplicacion de operadores aritmeticos utilizando, ingreso de datos, y seleccion con switch
+        int num1;
+        int num2;
+        int opcion;
+        Scanner Input = new Scanner (System.in);
+        
+        System.out.println("Bienvenido a operaciones aritmeticas para 2 valores");
+        System.out.println("Ingrese 1 para sumar ");
+        System.out.println("Ingrese 2 para restar ");
+        System.out.println("Ingrese 3 para dividir ");
+        System.out.println("Ingrese 4 para multiplicar");
+        
+        System.out.println("Que operacion desea hacer?");
+        opcion = Input.nextInt();
+        System.out.println("Ingrese primer numero");
+        num1 = Input.nextInt();
+        System.out.println("Ingrese segundo numero");
+        num2 = Input.nextInt();
+        
+        switch (opcion) 
+        {
+             case 1: 
+        int resultado = num1+num2;
+                 System.out.println("El resultado de la suma es " + resultado);
+              break;
+              case 2: 
+                 resultado = num1-num2;
+                 System.out.println("El resultado de la resta es " + resultado);
+              break;
+              case 3: 
+                 resultado = num1/num2;
+                 System.out.println("El resultado de la division es " + resultado);
+              break;
+              case 4: 
+                 resultado = num1*num2;
+                 System.out.println("El resultado de la multiplicacion es " + resultado);
+              break;
+
+    default:
+        break;
+         }
         
     }
     
